41eac4aba3a6e8be475e0bfab2a86f6de668fae3
Gökhan Akbaş <gkhn.akbs5050@gmail.com>
2023-12-15 17:45:02 +0300

    15.12.2023

--- app/src/main/java/com/gokhanakbas/veritabanproje/Profile_Settings.java
+++ app/src/main/java/com/gokhanakbas/veritabanproje/Profile_Settings.java
@@ -2,13 +2,39 @@ package com.gokhanakbas.veritabanproje;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Toast;
+
+import com.gokhanakbas.veritabanproje.databinding.ActivityProfileSettingsBinding;
 
 public class Profile_Settings extends AppCompatActivity {
 
+    ActivityProfileSettingsBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_settings);
+        binding=ActivityProfileSettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.cancelButtonProfileSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        binding.saveChangesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"Değişiklikler Kaydedildi",Toast.LENGTH_LONG).show();
+                Intent intent=new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/gokhanakbas/veritabanproje/fragment/ProfilePageFragment.java
+++ app/src/main/java/com/gokhanakbas/veritabanproje/fragment/ProfilePageFragment.java
@@ -1,21 +1,38 @@
 package com.gokhanakbas.veritabanproje.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.gokhanakbas.veritabanproje.Profile_Settings;
 import com.gokhanakbas.veritabanproje.R;
+import com.gokhanakbas.veritabanproje.databinding.FragmentProfilePageBinding;
 
 public class ProfilePageFragment extends Fragment {
+
+    FragmentProfilePageBinding binding;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_page, container, false);
+        binding=FragmentProfilePageBinding.inflate(inflater);
+
+        binding.setProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(v.getContext(), Profile_Settings.class);
+                startActivity(intent);
+                
+            }
+        });
+        return binding.getRoot();
     }
 }
